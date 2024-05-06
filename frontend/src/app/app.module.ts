239b23725ee6bfb264cc53548f36b159f0a79741
QuocAnh <anhbr2016@gmail.com>
2024-05-06 15:43:35 +0700

    form signup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,9 +9,14 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 registerLocaleData(en);
 
@@ -26,7 +31,15 @@ registerLocaleData(en);
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+        // NG ZORRO IMPORTS
+        NzSpinModule,
+        NzFormModule,
+        NzButtonModule,
+        NzInputModule,
+        NzLayoutModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
--- frontend/src/app/auth/components/signup/signup.component.ts
+++ frontend/src/app/auth/components/signup/signup.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
-  constructor() { }
+  isSpinning: boolean = false;
+  signupForm!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      checkPassword: [null, [Validators.required]],
+    })
+  }
+
+  register() {
+    console.log(this.signupForm.value);
   }
 
 }
